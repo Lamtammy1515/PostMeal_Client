37532e9cfee39fd5f70d42b15364f2c11e8434ea
Tammy Lam <lamtammy1515@gmail.com>
2021-05-15 23:33:02 -0500

    Creating an about & home page

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const Home = () => {
+        return (
+            <div>
+                Home Page
+            </div>
+        )
+    }
+
+export default Home; 
